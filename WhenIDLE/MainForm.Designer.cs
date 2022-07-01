592e28b6d0da5321dab452db3f1b72774a34283e
WillingMost7 <elay.hadad@gmail.com>
2022-07-01 03:07:19 +0300

    Add project files.

---
+++ WhenIDLE/MainForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WhenIDLE
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_trigger = new System.Windows.Forms.TextBox();
+            this.btn_setTrigger = new System.Windows.Forms.Button();
+            this.chkbx_autoStart = new System.Windows.Forms.CheckBox();
+            this.chkbx_runInBackground = new System.Windows.Forms.CheckBox();
+            this.timer_checkIDLE = new System.Windows.Forms.Timer(this.components);
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer_restartCheck = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "set maximum memory usage to trigger idle state";
+            // 
+            // tb_trigger
+            // 
+            this.tb_trigger.Location = new System.Drawing.Point(246, 12);
+            this.tb_trigger.Name = "tb_trigger";
+            this.tb_trigger.Size = new System.Drawing.Size(100, 20);
+            this.tb_trigger.TabIndex = 1;
+            // 
+            // btn_setTrigger
+            // 
+            this.btn_setTrigger.Location = new System.Drawing.Point(271, 38);
+            this.btn_setTrigger.Name = "btn_setTrigger";
+            this.btn_setTrigger.Size = new System.Drawing.Size(75, 23);
+            this.btn_setTrigger.TabIndex = 2;
+            this.btn_setTrigger.Text = "set";
+            this.btn_setTrigger.UseVisualStyleBackColor = true;
+            this.btn_setTrigger.Click += new System.EventHandler(this.btn_setTrigger_Click);
+            // 
+            // chkbx_autoStart
+            // 
+            this.chkbx_autoStart.AutoSize = true;
+            this.chkbx_autoStart.Location = new System.Drawing.Point(16, 86);
+            this.chkbx_autoStart.Name = "chkbx_autoStart";
+            this.chkbx_autoStart.Size = new System.Drawing.Size(118, 17);
+            this.chkbx_autoStart.TabIndex = 3;
+            this.chkbx_autoStart.Text = "start when pc starts";
+            this.chkbx_autoStart.UseVisualStyleBackColor = true;
+            this.chkbx_autoStart.CheckedChanged += new System.EventHandler(this.chkbx_autoStart_CheckedChanged);
+            // 
+            // chkbx_runInBackground
+            // 
+            this.chkbx_runInBackground.AutoSize = true;
+            this.chkbx_runInBackground.Location = new System.Drawing.Point(211, 86);
+            this.chkbx_runInBackground.Name = "chkbx_runInBackground";
+            this.chkbx_runInBackground.Size = new System.Drawing.Size(135, 17);
+            this.chkbx_runInBackground.TabIndex = 4;
+            this.chkbx_runInBackground.Text = "start in the background";
+            this.chkbx_runInBackground.UseVisualStyleBackColor = true;
+            // 
+            // timer_checkIDLE
+            // 
+            this.timer_checkIDLE.Interval = 1000;
+            this.timer_checkIDLE.Tick += new System.EventHandler(this.timer_checkIDLE_Tick);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(274, 124);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 5;
+            this.btn_start.Text = "start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(12, 124);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 6;
+            this.btn_stop.Text = "stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(102, 26);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.closeToolStripMenuItem.Text = "close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // timer_restartCheck
+            // 
+            this.timer_restartCheck.Interval = 1000;
+            this.timer_restartCheck.Tick += new System.EventHandler(this.timer_restartCheck_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 159);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.chkbx_runInBackground);
+            this.Controls.Add(this.chkbx_autoStart);
+            this.Controls.Add(this.btn_setTrigger);
+            this.Controls.Add(this.tb_trigger);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_trigger;
+        private System.Windows.Forms.Button btn_setTrigger;
+        private System.Windows.Forms.CheckBox chkbx_autoStart;
+        private System.Windows.Forms.CheckBox chkbx_runInBackground;
+        private System.Windows.Forms.Timer timer_checkIDLE;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Timer timer_restartCheck;
+    }
+}
+
---
+++ WhenIDLE/MainForm.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhenIDLE
+{
+    public partial class MainForm : Form
+    {
+        private static Int64 trigger;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            #region auto start chkbx
+            if (Registry.CurrentUser.OpenSubKey(StartupKey, true).GetValueNames().Contains(StartupValue))
+                chkbx_autoStart.Checked = true;
+            else
+                chkbx_autoStart.Checked = false;
+            #endregion
+
+            #region set IDLE trigger value
+            try
+            {
+                using (StreamReader sr = File.OpenText("settings"))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        trigger = Convert.ToInt64(s);
+                    }
+                }
+            }
+            catch (Exception ex) { }
+            #endregion
+        }
+
+        private void timer_checkIDLE_Tick(object sender, EventArgs e)
+        {
+            decimal percentOccupied = 100 - ((decimal)PerformanceInfo.GetPhysicalAvailableMemoryInMiB() / (decimal)PerformanceInfo.GetTotalMemoryInMiB() * 100);
+            if (trigger <= percentOccupied)
+            {
+                #region start idle
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "Rundll32.exe advapi32.dll,ProcessIdleTasks";
+                process.StartInfo = startInfo;
+                process.Start();
+                #endregion
+
+                timer_checkIDLE.Stop();
+                timer_restartCheck.Start();
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            timer_checkIDLE.Start();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            timer_checkIDLE.Stop();
+        }
+
+        #region auto start program when pc starts
+        //Startup registry key and value
+        private static readonly string StartupKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+        private static readonly string StartupValue = "WhenIDLE";
+        private void chkbx_autoStart_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkbx_autoStart.Checked)
+            {
+                //Set the application to run at startup
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(StartupKey, true);
+                key.SetValue(StartupValue, Application.ExecutablePath.ToString());
+            }
+            else
+            {
+                RegistryKey hklm = Microsoft.Win32.Registry.CurrentUser;
+                RegistryKey subkey = hklm.OpenSubKey(StartupKey, true);
+                subkey.DeleteValue(StartupValue);
+            }
+        }
+        #endregion
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer_restartCheck_Tick(object sender, EventArgs e)
+        {
+            decimal percentOccupied = 100 - ((decimal)PerformanceInfo.GetPhysicalAvailableMemoryInMiB() / (decimal)PerformanceInfo.GetTotalMemoryInMiB() * 100);
+            if (percentOccupied > trigger)
+            {
+                timer_checkIDLE.Start();
+                timer_restartCheck.Stop();
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_setTrigger_Click(object sender, EventArgs e)
+        {
+            // Check if file already exists. If yes, delete it.     
+            if (File.Exists("settings"))
+            {
+                File.Delete("settings");
+            }
+
+            // Create a new file     
+            using (FileStream fs = File.Create("settings"))
+            {
+                // Add some text to file    
+                byte[] txt = new UTF8Encoding(true).GetBytes(tb_trigger.Text);
+                fs.Write(txt, 0, txt.Length);
+            }
+            MessageBox.Show("restart the app to apply new settings");
+        }
+    }
+
+    public static class MouseMovements
+    {
+        public struct POINTAPI
+        {
+            public int x; public int y;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int GetCursorPos(ref POINTAPI lpPoint);
+
+        public static POINTAPI p = new POINTAPI();
+
+        public static int GetMyCusorPosX()
+        {
+            GetCursorPos(ref p);
+            return p.x;
+        }
+        public static int GetMyCusorPosY()
+        {
+            GetCursorPos(ref p);
+            return p.y;
+        }
+    }
+
+    public static class PerformanceInfo
+    {
+        [DllImport("psapi.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetPerformanceInfo([Out] out PerformanceInformation PerformanceInformation, [In] int Size);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PerformanceInformation
+        {
+            public int Size;
+            public IntPtr CommitTotal;
+            public IntPtr CommitLimit;
+            public IntPtr CommitPeak;
+            public IntPtr PhysicalTotal;
+            public IntPtr PhysicalAvailable;
+            public IntPtr SystemCache;
+            public IntPtr KernelTotal;
+            public IntPtr KernelPaged;
+            public IntPtr KernelNonPaged;
+            public IntPtr PageSize;
+            public int HandlesCount;
+            public int ProcessCount;
+            public int ThreadCount;
+        }
+
+        public static Int64 GetPhysicalAvailableMemoryInMiB()
+        {
+            PerformanceInformation pi = new PerformanceInformation();
+            if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+            {
+                return Convert.ToInt64((pi.PhysicalAvailable.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public static Int64 GetTotalMemoryInMiB()
+        {
+            PerformanceInformation pi = new PerformanceInformation();
+            if (GetPerformanceInfo(out pi, Marshal.SizeOf(pi)))
+            {
+                return Convert.ToInt64((pi.PhysicalTotal.ToInt64() * pi.PageSize.ToInt64() / 1048576));
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
